dd830d5b97fa019a64c029e2c796e0e891921af1
Anna Karutina <anna.karutina@khk.ee>
2022-01-13 08:46:57 +0200

    Person klass ja tema objektid

---
+++ praks2/person.py
@@ -0,0 +1,9 @@
+class Person():
+    # atribuudid
+    name = ''
+    age = 0
+    # meetodid
+    def info(self):
+        print('Person data: ')
+        print('name = ' + str(self.name))
+        print('age = ' + str(self.age))
---
+++ praks2/persons.py
@@ -0,0 +1,11 @@
+from praks2.person import Person
+
+anna = Person()
+anna.name = 'Anna'
+anna.age = 41
+anna.info()
+
+margus = Person()
+margus.name = 'Margus'
+margus.age = 45
+margus.info()
